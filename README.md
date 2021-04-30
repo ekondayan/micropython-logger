8f9e251d0af8053cd452c14075f760051530232f
Emil Kondayan <sogunxk4u@relay.firefox.com>
2021-04-30 18:45:28 +0300

    Implement missing FileHandler functions
    * FileHandler 
      - implement logrotate 
      - rename log_reset to delete_logs
      - implement delete_logs
    * add a simple example into the docs

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 micropython-logger
+
+
+
+At this point only a humble example is available
+
+```python
+from lib.logger import Logger, defs as logdefs
+from lib.logger.file import LogFile
+from lib.logger.console import LogConsole
+from lib.logger.syslog import LogSyslog
+
+log = Logger()
+
+log.add_handler(LogConsole(level = Logger.level_from_str('DEBUG')))
+log.add_handler(LogSyslog('syslog1', level = Logger.level_from_str('WARNING'), ip = '127.0.0.1', port = 514, appname = 'YourAppNameHere', log_format = LogSyslog.FORMAT_RFC3164))
+log.add_handler(LogSyslog('syslog2', level = Logger.level_from_str('WARNING'), ip = '127.0.0.1', port = 514, appname = 'YourAppNameHere', log_format = LogSyslog.FORMAT_RFC5424))
+log.add_handler(LogFile('system1', level = Logger.level_from_str('INFO'), file_path = '/', file_size_limit = 4096, file_count = 3))
+
+log.debug('Test debug')
+log.info('Test info')
+log.notice('Test notice')
+log.warn('Test warn')
+log.error('Test error')
+log.critical('Test critical')
+log.alert('Test alert')
+log.emergency('Test emergency')
+
+log.handlers[3].delete_logs()
+file_handler = log.get_handler('system1').delete_logs()
+```
--- src/file.py
+++ src/file.py
@@ -1,27 +1,69 @@
+import uos
+
 from .defs import *
 from .handler import LogHandler
 
 class LogFile(LogHandler):
     _line_format = '{timestamp} [{level}] {sys}{context} {err_title}{msg}'
 
-    def __init__(self, name, level = L_WARNING, path: str = '/', max_size: int = 20480):
+    def __init__(self, name, level = L_WARNING, file_path: str = '/', file_size_limit: int = 4096, file_count: int = 3):
+        if not isinstance(file_count, int) or not 1 <= file_count <= 99:
+            raise ValueError('Invalid parameter: file_count={} must be in range 1-99'.format(file_count))
+        elif not isinstance(file_size_limit, int) or not file_count > 0:
+            raise ValueError('Invalid parameter: file_size_limit={} must be greater than 0'.format(file_size_limit))
+
         super().__init__(name, level)
 
-        self._file_path = '{}/{}.log'.format(path.rstrip('/'), name)
-        self._max_size = max_size
-        self._file = open(self._file_path, 'at')
+        self._file_size_limit = file_size_limit
+        self._file_count = file_count
+
+        self._file_full_path = '{}/{}.log'.format(file_path.rstrip('/'), name)
+        self._file = open(self._file_full_path, 'at')
 
     def __del__(self):
         self._file.close()
 
     def send(self, level, msg, sys = None, context = None, error_id = None):
+        try:
+            uos.stat(self._file_full_path)
+        except OSError:
+            self._file = open(self._file_full_path, 'at')
+
         line = self._prepare_line(level, msg, sys, context, error_id)
         if line is not None:
             print(line, file = self._file)
             self._file.flush()
+            size = uos.stat(self._file_full_path)[6]
+            if size > self._file_size_limit:
+                self.log_rotate()
 
     def log_rotate(self):
-        pass
+        for i in range(self._file_count - 1, 0, -1):
+            old_filename = '{}.{}'.format(self._file_full_path, i)
+            new_filename = '{}.{}'.format(self._file_full_path, i + 1)
+            try:
+                uos.rename(old_filename, new_filename)
+            except:
+                pass
 
-    def log_reset(self):
-        pass
+        try:
+            uos.remove('{}.{}'.format(self._file_full_path, self._file_count))
+        except:
+            pass
+
+        self._file.close()
+        uos.rename(self._file_full_path, self._file_full_path + '.1')
+
+        self._file = open(self._file_full_path, 'at')
+
+    def delete_logs(self):
+        try:
+            uos.remove(self._file_full_path)
+        except:
+            pass
+
+        for i in range(1, self._file_count):
+            try:
+                uos.remove('{}.{}'.format(self._file_full_path, i))
+            except:
+                pass
